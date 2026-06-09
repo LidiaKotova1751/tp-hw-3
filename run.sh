ac5de0ed321debf737e77e4ee606f321991ac32b
Крылова Лидия <lbkrylova@edu.hse.ru>
2026-06-09 22:31:53 +0300

    Добавлены вспомогательные команды в run.sh

--- run.sh
+++ run.sh
@@ -10,13 +10,30 @@ elif [ "$command" = "run_generator" ]; then
     docker run --rm --mount type=bind,source="$(pwd)/data",target=/data hw3-generator
 
 elif [ "$command" = "create_local_data" ]; then
+    mkdir -p local_data
     py ./generator/generate.py ./local_data
 
 elif [ "$command" = "build_reporter" ]; then
     docker build -t hw3-reporter ./reporter
 
 elif [ "$command" = "run_reporter" ]; then
+    mkdir -p data
     docker run --rm --mount type=bind,source="$(pwd)/data",target=/data hw3-reporter
+
+elif [ "$command" = "structure" ]; then
+    find . -path "./.git" -prune -o -print
+
+elif [ "$command" = "clear_data" ]; then
+    mkdir -p data
+    find data -maxdepth 1 -type f \( -name "*.csv" -o -name "*.html" \) -delete
+
+elif [ "$command" = "inside_generator" ]; then
+    mkdir -p data
+    docker run --rm --entrypoint ls --mount type=bind,source="$(pwd)/data",target=/data hw3-generator -la //data
+
+elif [ "$command" = "inside_reporter" ]; then
+    mkdir -p data
+    docker run --rm --entrypoint ls --mount type=bind,source="$(pwd)/data",target=/data hw3-reporter -la //data
 
 else
     echo "Доступные команды:"
@@ -25,4 +42,9 @@ else
     echo "./run.sh create_local_data"
     echo "./run.sh build_reporter"
     echo "./run.sh run_reporter"
+    echo "./run.sh structure"
+    echo "./run.sh clear_data"
+    echo "./run.sh inside_generator"
+    echo "./run.sh inside_reporter"
+    exit 1
 fi
